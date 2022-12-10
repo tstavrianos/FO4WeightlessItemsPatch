f1dc5e06a9bc6e6e74d7ff0bd9ccd710d12baf72
Theodore Stavrianos <theodore@theo-s.eu>
2022-12-10 18:55:11 +0000

    first commit

---
+++ FO4WeightlessItemsPatch/Settings.cs
@@ -0,0 +1,26 @@
+﻿using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace FO4WeightlessItemsPatch
+{
+    internal class Settings
+    {
+        [MaintainOrder] [SettingName("Ingestibles")]
+        public bool Ingestibles = true;
+        [MaintainOrder] [SettingName("Ammunition")]
+        public bool Ammunition = true;
+        [MaintainOrder] [SettingName("Books/Magazines")]
+        public bool BooksMagazines = true;
+        [MaintainOrder] [SettingName("Keys/Passcards")]
+        public bool KeysPasscards = true;
+        [MaintainOrder] [SettingName("Notes/Holotapes")]
+        public bool NotesHolotapes = true;
+        [MaintainOrder] [SettingName("Misc (Mods only)")]
+        public bool Mods = true;
+        [MaintainOrder] [SettingName("Misc (All)")]
+        public bool Misc = false;
+        [MaintainOrder] [SettingName("Armors")]
+        public bool Armors = false;
+        [MaintainOrder] [SettingName("Weapons")]
+        public bool Weapons = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Port of Weightless Items from M8r98a4f2's Complex Item Sorter by M8r98a4f2 to Synthesis.
